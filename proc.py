1d7ba5d3343b43501d2b676bcca949b7cf529d33
Jeff Anderson <jefferya@programmerq.net>
2020-08-12 12:22:11 -0400

    better generator that handles titles and categories better

--- proc.py
+++ proc.py
@@ -2,9 +2,20 @@
 
 from datetime import datetime
 import html
+import unicodedata, re, io
+
+def slugify(value):
+    """
+    Converts to lowercase, removes non-word characters (alphanumerics and
+    underscores) and converts spaces to hyphens. Also strips leading and
+    trailing whitespace.
+    """
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    return re.sub('[-\s]+', '-', value)
 
 #f = open("Untitled spreadsheet - Sheet2.tsv", "r")
-f = open("Namo Episodes - Sheet2 (1).tsv", "r")
+f = io.open("Namo Episodes - Sheet2 (3).tsv", "r", encoding="utf-8")
 f.readline() # headers
 
 for line in f:
@@ -12,22 +23,22 @@ for line in f:
     realdate=datetime.strptime(date2.strip(), '%m/%d/%Y %H:%M:%S')
     #print(title, url, realdate)
     if filename!="":
-        outfile =  '-'.join([str(realdate.year), str(realdate.month), str(realdate.day)] +  title.lower().split(' ')[:2]) + '.markdown'
-        frontmatter='''---
+        outfile =  '-'.join([realdate.strftime('%Y-%m-%d'), slugify(unicode(title))]) + '.markdown'
+        frontmatter=u'''---
 layout: post
 title: >
     {title}
 episode_url: {url}
 libsyn_url: {lurl}
-category: History
+category: {category}
 date: {date}
 ---
 
-'''.format(title=html.escape(title),url=url,lurl=lurl,date=realdate.strftime('%Y-%m-%d %H:%M:%S +0000'))
+'''.format(title=title,url=url,lurl=lurl,category=category,date=realdate.strftime('%Y-%m-%d %H:%M:%S +0000'))
 
         print ("doing ", title)
-        o=open('output/'+outfile, "w")
-        gen=open(filename, 'r')
+        o=io.open('output/'+outfile, "w", encoding='utf-8')
+        gen=io.open(filename, 'r', encoding='utf-8')
         o.write(frontmatter)
         o.write(gen.read())
         gen.close()
